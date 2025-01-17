85dc341dda2d44572dc8efbbb86fbd3cfdfdb4b3
Tristan-Vo <votristan777@gmail.com>
2025-01-16 21:34:29 -0500

    feat: Add additional books to BookCatalogue and enhance Item tests with author verification

--- src/BookCatalogueTest.java
+++ src/BookCatalogueTest.java
@@ -20,7 +20,9 @@ public class BookCatalogueTest {
         actual.add(new Book("The Lord of the Rings", "J.R.R. Tolkien"));
         actual.add(new Book("To Kill a Mockingbird", "Harper Lee"));
         actual.add(new Book("Diary of a Wimpy Kid: Dog Days", "Jeff Kinney"));
-        
+        actual.add(new Book("The Catcher in the Rye", "J.D. Salinger"));
+        actual.add(new Book("The Hobbit", "J.R.R. Tolkien"));
+        actual.add(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling"));
 
         // Act
         List<Book> expected = catalogue.getBooks();
--- src/ItemTest.java
+++ src/ItemTest.java
@@ -1,10 +1,23 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class ItemTest {
     @Test
     void testGetAuthor() {
+
+        // Arrange
+        Item item = new Item("The Great Gatsby", "F. Scott Fitzgerald");
+        String actual = item.getAuthor();
         
+        // Act
+        String expected = "F. Scott Fitzgerald";
+
+        // Assert
+        assertEquals(actual, expected);
     }
+
+
 
     @Test
     void testGetTitle() {
